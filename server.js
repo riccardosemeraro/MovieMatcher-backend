5e6080031d368bc3e874c8dcb0b832bbd9900efa
Riccardo Semeraro <150936465+riccardosemeraro@users.noreply.github.com>
2024-08-03 11:28:46 +0000

    server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // server.js
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const jwt = require('jsonwebtoken');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 9000; //process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
@@ -12,23 +13,42 @@ app.use(bodyParser.json());
 // Secret key for JWT
 const SECRET_KEY = 'your_secret_key';
 
+app.get('/', (req, res) => {
+    console.log('Hello World');
+    //res.send('Hello World');
+});
+
+
 //Route per verificare se l'utente è autenticato
 app.post('/verify', (req, res) => {
-    const token = req.body.token;
+    const user = req.body;
 
-    if (!token) {
-        return res.status(400).json({ message: 'Token is required' });
+    console.log('Dati ricevuti:', { user });
+
+    if (!user) {
+        return res.status(400).json({ message: 'User is required' });
     }
 
     try {
-        const decoded = jwt.verify(token, SECRET_KEY);
-        return res.status(200).json({ message: 'User is authenticated', user: decoded });
+        //simulazione script di verifica token
+
+        return res.status(200).json({ message: 'User is authenticated', user });
     } catch (error) {
         return res.status(401).json({ message: 'Invalid token' });
     }
 });
 
+
+
+
+
+
+
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log(`Server avviato su http://localhost:${PORT}`);
+
 });
