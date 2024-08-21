ce4c541379bb4349959ae2910c6948f643b1eb1c
riccardosemeraro <riccardosemer@gmail.com>
2024-08-21 23:46:55 +0200

    add and remove film

--- Controllers/databaseConnectionController.js
+++ Controllers/databaseConnectionController.js
@@ -1,5 +1,7 @@
 const { application } = require('express');
 const User = require('../Models/Users');
+const { use } = require('moongose/routes');
+const e = require('express');
 
 const verify = async (req, res) => {
     console.log("Verifica che l'utente sia in MongoDB");
@@ -63,9 +65,137 @@ const verify = async (req, res) => {
 
 };
 
-//const ktm = ...;
+const filmCheckList = async (req, res) => {
+    console.log("Verifica che un film sia in lista");
+    const userSub = req.body.body.userSub;
+    const movieId = req.body.body.movieId;
+    const list = req.body.body.list;
+    const token = req.headers.authorization;
+
+    if (!token && !userSub && !movieId && !list) {
+        res.status(401).json({ message: 'Params missing' });
+    }
+
+    try {
+        //simulazione script di verifica che il film sia in lista
+
+        const user = await User.Users.findById(userSub);
+
+        if (user === null) {
+            res.status(404).json({ message: 'User not found' });
+        } else {
+            let film = undefined;
+            if (list === 'visti') {
+                film = user.MyList.find((movie) => movie === movieId);
+            } else if (list === 'vedere') {
+                film = user.WatchList.find((movie) => movie === movieId);
+            }
+
+            if (film === undefined) {
+                res.status(200).json({ value: true, message: 'Film not found in list' });
+            }
+            else {
+                res.status(200).json({ value: false, message: 'Film found in list'});
+            }
+        }
+    } catch (error) {
+        console.error("Errore durante la verifica del film:", error);
+        res.status(500).json({ message: 'Error checking film', error });
+    }
+};
+
+const addFilm = async (req, res) => {
+    console.log("Aggiungi o rimuovi un film dalla lista");
+    
+    const userSub = req.body.body.userSub;
+    const movieId = req.body.body.movieId;
+    const list = req.body.body.list;
+
+    const token = req.headers.authorization;
+
+    if (!token && !userSub && !movieId && !list && !insert) {
+        res.status(401).json({ message: 'Params missing' });
+    }
+
+    try {
+        const user = await User.Users.findById(userSub);
+
+        if (user === null) {
+            res.status(404).json({ message: 'User not found' });
+        } else {
+            if(list === 'visti') {
+                if (!user.WatchList.includes(movieId) && !user.MyList.includes(movieId)) {
+                    user.MyList.push(movieId);
+                    //si deve aggiornare anche il database
+                    await User.Users.findByIdAndUpdate(userSub, { MyList: user.MyList });
+                    res.status(200).json({ valueState: false, message: 'Film added to list', list: 'visti'});
+                }
+                else {
+                    res.status(200).json({ valueState: true, message: 'Film already in list', list: 'visti'});
+                }
+            } else if (list === 'vedere') {
+                if (!user.WatchList.includes(movieId) && !user.MyList.includes(movieId)) {
+                    user.WatchList.push(movieId);
+                    await User.Users.findByIdAndUpdate(userSub, { WatchList: user.WatchList });
+                    res.status(200).json({ valueState: false, message: 'Film added to list', list: 'vedere'});
+                }
+                else {
+                    res.status(200).json({ valueState: true, message: 'Film already in list', list: 'vedere'});
+                }
+            }
+        }
+
+    } catch (error) {
+        console.error("Errore durante l'aggiunta o rimozione del film:", error);
+        res.status(500).json({ message: 'Error adding or removing film', error });
+    }
+};
+
+const removeFilm = async (req, res) => {
+    console.log("Aggiungi o rimuovi un film dalla lista");
+    
+    const userSub = req.body.body.userSub;
+    const movieId = req.body.body.movieId;
+    const list = req.body.body.list;
+
+    const token = req.headers.authorization;
+
+    if (!token && !userSub && !movieId && !list && !insert) {
+        res.status(401).json({ message: 'Params missing' });
+    }
+
+    try {
+        const user = await User.Users.findById(userSub);
+
+        if (user === null) {
+            res.status(404).json({ message: 'User not found' });
+        } else {
+            if(list === 'visti') {
+                if (user.MyList.includes(movieId)) {
+                    user.MyList = user.MyList.filter((movie) => movie !== movieId);
+                    await User.Users.findByIdAndUpdate(userSub, { MyList: user.MyList });
+                    res.status(200).json({ valueState: true, message: 'Film removed from list', list: 'visti'});
+                }
+            } else if (list === 'vedere') {
+                if (user.WatchList.includes(movieId)) {
+                    user.WatchList = user.WatchList.filter((movie) => movie !== movieId);
+                    await User.Users.findByIdAndUpdate(userSub, { WatchList: user.WatchList });
+                    res.status(200).json({ valueState: true, message: 'Film removed from list', list: 'vedere'});
+                }
+            }
+        }
+
+    } catch (error) {
+        console.error("Errore durante l'aggiunta o rimozione del film:", error);
+        res.status(500).json({ message: 'Error adding or removing film', error });
+    }
+};
+
 
 module.exports =
                 {
-                    verify
-                }; //ktm, ...
+                    verify,
+                    filmCheckList,
+                    addFilm,
+                    removeFilm
+                };
--- Routes/databaseConnection.js
+++ Routes/databaseConnection.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const controllerDatabase = require('../Controllers/databaseConnectionController');
 
 router.post('/verify', controllerDatabase.verify);
+router.post('/filmCheckList', controllerDatabase.filmCheckList);
+router.post('/addFilm', controllerDatabase.addFilm);
+router.post('/removeFilm', controllerDatabase.removeFilm);
 
 
 
